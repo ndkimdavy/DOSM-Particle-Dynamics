7e38f25c0986022258dc3337aa928464417e9280
ndkd <ndkimdavy@gmail.com>
2026-01-08 02:20:46 +0100

    fix precision *.csv and remove std::pow

--- src/dosmLawLennardJones.cpp
+++ src/dosmLawLennardJones.cpp
@@ -1,65 +1,68 @@
 #include "dosmLawLennardJones.hpp"
-#include <cmath>
 
 namespace dosm
 {
-	DosmLawLennardJones::DosmLawLennardJones(vector_t<DosmParticle>& particles, r64_t sigma, r64_t epsilon):
-		particles(particles)
-	{
-		this->sigma = sigma;
-		this->epsilon = epsilon;
-	}
+    DosmLawLennardJones::DosmLawLennardJones(vector_t<DosmParticle>& particles, r64_t sigma, r64_t epsilon):
+        particles(particles)
+    {
+        this->sigma = sigma;
+        this->epsilon = epsilon;
+    }
 
-	void DosmLawLennardJones::kernel(Result* result)
-	{
-		idx_t n = particles.size();
+    void DosmLawLennardJones::kernel(Result* result)
+    {
+        idx_t n = particles.size();
 
-		r64_t energy = 0.0;
-		for (auto& particle : particles)
-		{
-			particle.p_energy = 0.0;
-			particle.force(0) = particle.force(1) = particle.force(2) = 0.0;
-		}
+        r64_t energy = 0.0;
+        for (auto& particle : particles)
+        {
+            particle.p_energy = 0.0;
+            particle.force(0) = particle.force(1) = particle.force(2) = 0.0;
+        }
 
-		for (idx_t i = 0; i < n; ++i)
-		{
-			for (idx_t j = i + 1; j < n; ++j)
-			{
-				r64_t dx = particles[i].position(0) - particles[j].position(0);
-				r64_t dy = particles[i].position(1) - particles[j].position(1);
-				r64_t dz = particles[i].position(2) - particles[j].position(2);
-				r64_t r2 = dx*dx + dy*dy + dz*dz;
-				if (r2 == 0.0) continue;
-				r64_t sigma2 = sigma * sigma;
+        for (idx_t i = 0; i < n; ++i)
+        {
+            for (idx_t j = i + 1; j < n; ++j)
+            {
+                r64_t dx = particles[i].position(0) - particles[j].position(0);
+                r64_t dy = particles[i].position(1) - particles[j].position(1);
+                r64_t dz = particles[i].position(2) - particles[j].position(2);
+                r64_t r2 = dx*dx + dy*dy + dz*dz;
+                if (r2 == 0.0) continue;
+                r64_t sigma2 = sigma * sigma;
 
-				r64_t uij = 4.0 * epsilon * (std::pow(sigma2/r2, 6) - std::pow(sigma2/r2, 3));
-				energy += uij;
+                r64_t _invR2  = sigma2 / r2;
+                r64_t _invR6  = _invR2 * _invR2 * _invR2;
+                r64_t _invR12 = _invR6 * _invR6;
 
-				r64_t oij = 48.0 * epsilon * (std::pow(sigma2/r2, 6) - 0.5 * std::pow(sigma2/r2, 3));
-				r64_t fx = oij * (dx/r2);
-				r64_t fy = oij * (dy/r2);
-				r64_t fz = oij * (dz/r2);
+                r64_t uij = 4.0 * epsilon * (_invR12 - _invR6);
+                energy += uij;
 
-				particles[i].force(0) += fx;
-				particles[i].force(1) += fy;
-				particles[i].force(2) += fz;
+                r64_t oij = 48.0 * epsilon * (_invR12 - 0.5 * _invR6);
+                r64_t fx = oij * (dx/r2);
+                r64_t fy = oij * (dy/r2);
+                r64_t fz = oij * (dz/r2);
 
-				particles[j].force(0) -= fx;
-				particles[j].force(1) -= fy;
-				particles[j].force(2) -= fz;
+                particles[i].force(0) += fx;
+                particles[i].force(1) += fy;
+                particles[i].force(2) += fz;
 
-				// ui = uj = 1/2 uij (convention)
-				particles[i].p_energy+= 0.5 * uij;
-				particles[j].p_energy+= 0.5 * uij;
-			}
+                particles[j].force(0) -= fx;
+                particles[j].force(1) -= fy;
+                particles[j].force(2) -= fz;
 
-			// DOSM_PROGRESS("Lennard-Jones", i + 1, n, -1);
-		}
+                // ui = uj = 1/2 uij (convention)
+                particles[i].p_energy+= 0.5 * uij;
+                particles[j].p_energy+= 0.5 * uij;
+            }
 
-		result->energy  = energy;
-		result->particles = &particles;
+            // DOSM_PROGRESS("Lennard-Jones", i + 1, n, -1);
+        }
 
-	}
+        result->energy  = energy;
+        result->particles = &particles;
+
+    }
 
 } // namespace dosm
 
--- src/dosmLawLennardJonesPeriodic.cpp
+++ src/dosmLawLennardJonesPeriodic.cpp
@@ -2,74 +2,78 @@
 
 namespace dosm
 {
-	DosmLawLennardJonesPeriodic::DosmLawLennardJonesPeriodic(vector_t<DosmParticle>& particles, r64_t sigma, r64_t epsilon, r64_t boxLength, r64_t rayCut) : 
-		DosmLawLennardJones(particles, sigma, epsilon)  
-	{
-		this->boxLength = boxLength;
-		this->rayCut = rayCut;
-		images.reserve(27);
-		for (i32_t i = -1; i <= 1; ++i)
-			for (i32_t j = -1; j <= 1; ++j) 
-				for (i32_t k = -1; k <= 1; ++k)
-				{
-					tensor_t<r64_t, 3> image;
-					image(0) = i * boxLength;
-					image(1) = j * boxLength;
-					image(2) = k * boxLength;
-					images.push_back(image);
-				}
-	}
+    DosmLawLennardJonesPeriodic::DosmLawLennardJonesPeriodic(vector_t<DosmParticle>& particles, r64_t sigma, r64_t epsilon, r64_t boxLength, r64_t rayCut) : 
+        DosmLawLennardJones(particles, sigma, epsilon)  
+    {
+        this->boxLength = boxLength;
+        this->rayCut = rayCut;
+        images.reserve(27);
+        for (i32_t i = -1; i <= 1; ++i)
+            for (i32_t j = -1; j <= 1; ++j) 
+                for (i32_t k = -1; k <= 1; ++k)
+                {
+                    tensor_t<r64_t, 3> image;
+                    image(0) = i * boxLength;
+                    image(1) = j * boxLength;
+                    image(2) = k * boxLength;
+                    images.push_back(image);
+                }
+    }
 
-	void DosmLawLennardJonesPeriodic::kernel(Result* result)
-	{
-		idx_t n = particles.size();
+    void DosmLawLennardJonesPeriodic::kernel(Result* result)
+    {
+        idx_t n = particles.size();
 
-		r64_t energy = 0.0;
-		for (auto& particle : particles)
-		{
-			particle.p_energy = 0.0;
-			particle.force(0) = particle.force(1) = particle.force(2) = 0.0;
-		}
+        r64_t energy = 0.0;
+        for (auto& particle : particles)
+        {
+            particle.p_energy = 0.0;
+            particle.force(0) = particle.force(1) = particle.force(2) = 0.0;
+        }
 
-		for (auto& image : images)
-			for (idx_t i = 0; i < n; ++i)
-			{
-				for (idx_t j = i + 1; j < n; ++j)
-				{
-					r64_t dx = particles[i].position(0) - (particles[j].position(0) + image(0));
-					r64_t dy = particles[i].position(1) - (particles[j].position(1) + image(1));
-					r64_t dz = particles[i].position(2) - (particles[j].position(2) + image(2));
-					r64_t r2 = dx*dx + dy*dy + dz*dz;
-					if (r2 == 0.0 || r2 > rayCut * rayCut) continue;
-					r64_t sigma2 = sigma * sigma;
+        for (auto& image : images)
+            for (idx_t i = 0; i < n; ++i)
+            {
+                for (idx_t j = i + 1; j < n; ++j)
+                {
+                    r64_t dx = particles[i].position(0) - (particles[j].position(0) + image(0));
+                    r64_t dy = particles[i].position(1) - (particles[j].position(1) + image(1));
+                    r64_t dz = particles[i].position(2) - (particles[j].position(2) + image(2));
+                    r64_t r2 = dx*dx + dy*dy + dz*dz;
+                    if (r2 == 0.0 || r2 > rayCut * rayCut) continue;
+                    r64_t sigma2 = sigma * sigma;
 
-					r64_t uij = 4.0 * epsilon * (std::pow(sigma2/r2, 6) - std::pow(sigma2/r2, 3));
-					energy += uij;
+                    r64_t _invR2  = sigma2 / r2;
+                    r64_t _invR6  = _invR2 * _invR2 * _invR2;
+                    r64_t _invR12 = _invR6 * _invR6;
 
-					r64_t oij = 48.0 * epsilon * (std::pow(sigma2/r2, 6) - 0.5 * std::pow(sigma2/r2, 3));
-					r64_t fx = oij * (dx/r2);
-					r64_t fy = oij * (dy/r2);
-					r64_t fz = oij * (dz/r2);
+                    r64_t uij = 4.0 * epsilon * (_invR12 - _invR6);
+                    energy += uij;
 
-					particles[i].force(0) += fx;
-					particles[i].force(1) += fy;
-					particles[i].force(2) += fz;
+                    r64_t oij = 48.0 * epsilon * (_invR12 - 0.5 * _invR6);
+                    r64_t fx = oij * (dx/r2);
+                    r64_t fy = oij * (dy/r2);
+                    r64_t fz = oij * (dz/r2);
 
-					particles[j].force(0) -= fx;
-					particles[j].force(1) -= fy;
-					particles[j].force(2) -= fz;
+                    particles[i].force(0) += fx;
+                    particles[i].force(1) += fy;
+                    particles[i].force(2) += fz;
 
-					// ui = uj = 1/2 uij (convention)
-					particles[i].p_energy += 0.5 * uij;
-					particles[j].p_energy += 0.5 * uij;
-				}
+                    particles[j].force(0) -= fx;
+                    particles[j].force(1) -= fy;
+                    particles[j].force(2) -= fz;
 
-				// DOSM_PROGRESS("Lennard-Jones Periodic", i + 1, n, -);
-			}
+                    // ui = uj = 1/2 uij (convention)
+                    particles[i].p_energy += 0.5 * uij;
+                    particles[j].p_energy += 0.5 * uij;
+                }
 
-		result->energy    = energy;
-		result->particles = &particles;
+                // DOSM_PROGRESS("Lennard-Jones Periodic", i + 1, n, -);
+            }
 
-	}
+        result->energy    = energy;
+        result->particles = &particles;
+
+    }
 
 } // namespace dosm
